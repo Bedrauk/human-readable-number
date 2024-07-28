778eb346b04974d83710d37d2d76b542c7c1f6e0
Bedrauk <111381113+Bedrauk@users.noreply.github.com>
2024-07-28 14:51:10 +0300

    docs: add some code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function toReadable (number) {
-  console.log('hello world')
+    const exponents = ['thousand', 'million', 'billion'];
+    const nums = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight','nine', 'ten', 'eleven', 'twelve', 'thirteen'];
+    const postfix = ['teen', 'ty', 'twenty', 'thirty'];
+    let firstNum = Math.floor(number/100), midNum = Math.floor(number/10) % 10, lastNum = number % 10;
+    let res = '', exponent = 0, tmp = number;
+
+    if (firstNum) {
+        res = `${nums[firstNum]} hundred `
+    }
+    if (number > (nums.length-1) && number < 20) {
+        if (nums[lastNum].endsWith('o') || nums[lastNum].endsWith('t') || nums[lastNum].endsWith('ve')) {
+            return res + nums[lastNum].slice(0,-1) + postfix[0];
+        } else {
+            return res + nums[lastNum] + postfix[0];
+        }
+    } else {
+        return res + nums[number]
+    }
+
+    while (tmp) {
+        if (exponent) {
+            
+        } else {
+           
+        }
+        tmp = Math.floor(tmp/1000); 
+        ++exponent;
+    }
 }
